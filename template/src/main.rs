57bac697c8d3d8b9f17af6591c297e9297c82ff4
adrgs <albastroiudragos@gmail.com>
2023-12-03 00:50:31 +0100

    Add template

---
+++ template/src/main.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn part1(filename: &str) -> io::Result<()> {
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut ans = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        
+    }
+
+    println!("Part 1: {}", ans);
+
+    Ok(())
+}
+
+fn part2(filename: &str) -> io::Result<()> {
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut ans = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        
+    }
+
+    println!("Part 2: {}", ans);
+
+    Ok(())
+}
+
+fn main() {
+    part1("./src/input.txt").unwrap();
+    part2("./src/input.txt").unwrap();
+}
