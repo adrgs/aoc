c147108517f0d0cb3d8bc28c1c23f712b4ed955f
adrgs <albastroiudragos@gmail.com>
2023-12-10 22:36:48 +0100

    Add day8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::hash::Hash;
 use std::io::{self, BufRead};
 use std::path::Path;
 
@@ -45,10 +44,48 @@ fn part2(filename: &str) -> io::Result<()> {
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
-    let mut ans = 0;
+    let mut lines = reader.lines();
 
-    for line in reader.lines() {
-        let line = line.unwrap();
+    let steps = lines.next().unwrap()?;
+    let graph: HashMap<String, (String, String)> = lines
+        .filter_map(|line| {
+            let line = line.ok()?;
+            let (node, edges) = line.split_once(" = ")?;
+            let (edge1, edge2) = edges.split_once(", ")?;
+            let edge1 = edge1.trim_matches('(').to_string();
+            let edge2 = edge2.trim_matches(')').to_string();
+            Some((node.to_string(), (edge1, edge2)))
+        })
+        .collect();
+
+    let mut ans = 0;
+    let mut nodes: Vec<&str> = graph.keys().filter_map(|node| {
+        if node.ends_with("A") {
+            Some(node.as_str())
+        } else {
+            None
+        }
+    }).collect();
+
+    loop {
+        let mut good = true;
+        for i in 0..nodes.len() {
+            let node = nodes[i];
+
+            if steps.chars().nth(ans % steps.len()).unwrap() == 'R' {
+                nodes[i] = &graph[node].1;
+            } else {
+                nodes[i] = &graph[node].0;
+            }
+            
+            if !nodes[i].ends_with("Z") {
+                good = false;
+            }
+        }
+        ans += 1;
+        if good {
+            break;
+        }
     }
 
     println!("Part 2: {}", ans);
