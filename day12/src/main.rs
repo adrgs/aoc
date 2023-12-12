f08ac30bf77a845c6352aac5b69130188ca4a58a
adrgs <albastroiudragos@gmail.com>
2023-12-12 20:03:16 +0100

    Add day12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -64,9 +65,89 @@ fn part2(filename: &str) -> io::Result<()> {
 
     let mut ans = 0;
 
+    fn dp(row: &mut Vec<char>, conditions: &Vec<i64>, memo: &mut HashMap<(usize, usize), i64>, i: usize, j: usize) -> i64 {
+        if i >= row.len() && j >= conditions.len() { // empty row and empty conditions
+            return 1;
+        }
+        if i >= row.len() && j < conditions.len() { // empty row can't satisfy non-empty conditions
+            return 0;
+        }
+        if i < row.len() && j >= conditions.len() { // check if row doesn't contain any blocks
+            for k in i..row.len() {
+                if row[k] == '#' {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+        if memo.contains_key(&(i, j)) {
+            return memo[&(i, j)];
+        }
+        let mut ans = 0;
+
+        // find the largest X? block that is larger than the j'th condition
+        let mut k = i;
+        let mut sum = 0;
+        let mut contains_block = false;
+        while k < row.len() {
+            if row[k] == '?' || row[k] == '#' {
+                sum += 1;
+                if row[k] == '#' {
+                    contains_block = true;
+                }
+            } else {
+                if sum >= conditions[j] {
+                    break;
+                }
+                if contains_block {
+                    memo.insert((i, j), 0);
+                    return 0;
+                }
+                sum = 0;
+            }
+            k += 1;
+        }
+        if sum < conditions[j]{
+            memo.insert((i, j), 0);
+            return 0;
+        }
+        for s in (k-sum as usize)..(k - conditions[j] as usize + 1) {
+            if (s + conditions[j] as usize) < row.len() && row[s + conditions[j] as usize] == '#' {
+                if row[s] == '#' {
+                    contains_block = true;
+                    break;
+                }
+                continue;
+            }
+            ans += dp(row, conditions, memo, s + conditions[j] as usize + 1, j + 1);
+            if row[s] == '#' {
+                contains_block = true;
+                break;
+            }
+        }
+        
+        if !contains_block {
+            ans += dp(row, conditions, memo, k, j);
+        }
+
+        memo.insert((i, j), ans);
+        return ans;
+    }
+
     for line in reader.lines() {
         let line = line.unwrap();
         
+        let (row, conditions) = line.split_once(" ").unwrap();
+        let mut row: Vec<char> = row.chars().collect();
+        let mut conditions = conditions.split(',').map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        let mut memo: HashMap<(usize, usize), i64> = HashMap::new();
+
+        row.push('?');
+        row = row.repeat(5);
+        row.pop(); // remove last element
+        conditions = conditions.repeat(5);
+
+        ans += dp(&mut row, &conditions, &mut memo, 0, 0);
     }
 
     println!("Part 2: {}", ans);
