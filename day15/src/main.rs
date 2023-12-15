a1be300347002578aa66324bc984c2e48f4a3d55
adrgs <albastroiudragos@gmail.com>
2023-12-15 15:41:18 +0100

    Add day15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -39,9 +39,52 @@ fn part2(filename: &str) -> io::Result<()> {
 
     let mut ans = 0;
 
-    for line in reader.lines() {
-        let line = line.unwrap();
-        
+    let mut hashmap: Vec<Vec<(&str, i32)>> = Vec::new();
+    for _ in 0..256 {
+        hashmap.push(Vec::new());
+    }
+
+    let input = reader.lines().take(1).next().unwrap().unwrap();
+    for step in input.split(',') {
+        let mut elements: Vec<_> = step.split_terminator(&['-', '='][..]).collect();
+        if elements.len() < 2 {
+            elements.push("0");
+        }
+
+        for window in elements.windows(2) {
+            let label = window[0];
+            let value = window[1].parse::<i32>().unwrap_or_default();
+            let box_id = hash(label);
+            let add = step.find('=');
+            if add.is_some() {
+                let mut found = false;
+                for i in 0..hashmap[box_id as usize].len() {
+                    if hashmap[box_id as usize][i].0 == label {
+                        hashmap[box_id as usize][i].1 = value;
+                        found = true;
+                        break;
+                    }
+                }
+                if !found {
+                    hashmap[box_id as usize].push((label, value));
+                }
+            } else {
+                // remove label from hashmap
+                let mut new_hashmap = Vec::new();
+                for (lb, value) in hashmap[box_id as usize].iter() {
+                    if label != *lb {
+                        new_hashmap.push((*lb, *value));
+                    }
+                }
+                hashmap[box_id as usize] = new_hashmap;
+            }
+        }
+    }
+
+    for i in 0..hashmap.len() {
+        for j in 0..hashmap[i].len() {
+            ans += (i as i32 + 1) * (j as i32 + 1) * hashmap[i][j].1;
+        }
     }
 
     println!("Part 2: {}", ans);
