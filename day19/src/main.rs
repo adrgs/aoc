dfd857c37783bad9386bde387b9bdae480a4ca17
adrgs <albastroiudragos@gmail.com>
2023-12-19 16:01:16 +0100

    Add day19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -25,6 +25,12 @@ struct Part {
     s: i32
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Interval {
+    start: i32,
+    end: i32
+}
+
 trait GetPart {
     fn get(&self, field: char) -> i32;
 }
@@ -150,11 +156,131 @@ fn part2(filename: &str) -> io::Result<()> {
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
-    let mut ans = 0;
+    let mut rules: HashMap<String, Rule> = HashMap::new();
+    let mut parts: Vec<Part> = Vec::new();
+
+    let mut ans: i64 = 0;
+    let mut parse_rules = true;
 
     for line in reader.lines() {
         let line = line.unwrap();
+        if line == "" {
+            parse_rules = false;
+            continue;
+        }
         
+        if parse_rules {
+            let matches = regex::Regex::new(r"^(.*)\{(.*)\}$").unwrap().captures(&line).unwrap();
+            let rule_name = matches.get(1).unwrap().as_str();
+            let rule_conditions = matches.get(2).unwrap().as_str();
+
+            let mut conditions: Vec<Condition> = Vec::new();
+            let mut default = "";
+            for condition in rule_conditions.split(",") {
+                let (criterion, path) = condition.split_once(":").unwrap_or(("", condition));
+                if criterion == "" {
+                    default = path;
+                } else {
+                    let field = criterion.chars().nth(0).unwrap();
+                    let operator = criterion.chars().nth(1).unwrap();
+                    let value = criterion[2..].parse::<i32>().unwrap();
+                    let condition = Condition {
+                        value: value,
+                        criterion: operator,
+                        field: field,
+                        jump_to: path.to_string()
+                    };
+                    conditions.push(condition);
+                }
+            }
+            let rule = Rule {
+                conditions: conditions,
+                default: default.to_string()
+            };
+            rules.insert(rule_name.to_string(), rule);
+        } else {
+            let matches = regex::Regex::new(r"x=(\d+),m=(\d+),a=(\d+),s=(\d+)").unwrap().captures(&line).unwrap();
+            let x = matches.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let m = matches.get(2).unwrap().as_str().parse::<i32>().unwrap();
+            let a = matches.get(3).unwrap().as_str().parse::<i32>().unwrap();
+            let s = matches.get(4).unwrap().as_str().parse::<i32>().unwrap();
+            let part = Part {
+                x: x,
+                m: m,
+                a: a,
+                s: s
+            };
+            parts.push(part);
+        }
+    }
+
+    let mut queue: Vec<(&str, Interval, Interval, Interval, Interval)> = Vec::new();
+    queue.push(("in", Interval { start: 1, end: 4000 }, Interval { start: 1, end: 4000 }, Interval { start: 1, end: 4000 }, Interval { start: 1, end: 4000 }));
+
+    let mut accepted: Vec<(Interval, Interval, Interval, Interval)> = Vec::new();
+    let mut rejected: Vec<(Interval, Interval, Interval, Interval)> = Vec::new();
+
+    while queue.len() > 0 {
+        let (rule, x, m, a, s) = queue.pop().unwrap();
+
+        if x.start > x.end || m.start > m.end || a.start > a.end || s.start > s.end {
+            continue;
+        }
+
+        if rule == "A" {
+            // ans += (x.end - x.start + 1) as i64 * (m.end - m.start + 1) as i64 * (a.end - a.start + 1) as i64 * (s.end - s.start + 1) as i64;
+            accepted.push((x, m, a, s));
+            continue;
+        } else if rule == "R" {
+            rejected.push((x, m, a, s));
+            continue;
+        }
+
+        let rule = rules.get(rule).unwrap();
+
+        let mut x = x.clone();
+        let mut m = m.clone();
+        let mut a = a.clone();
+        let mut s = s.clone();
+        for condition in &rule.conditions {
+            let value = match condition.field {
+                'x' => &mut x,
+                'm' => &mut m,
+                'a' => &mut a,
+                's' => &mut s,
+                _ => panic!("Invalid field {}", condition.field)
+            };
+            let mut new_value = value.clone();
+            match condition.criterion {
+                '>' => {
+                    if condition.value > value.start {
+                        new_value.start = condition.value + 1;
+                        value.end = condition.value;
+                    }
+                },
+                '<' => {
+                    if condition.value < value.end {
+                        new_value.end = condition.value - 1;
+                        value.start = condition.value;
+                    }
+                },
+                _ => panic!("Invalid criterion {}", condition.criterion)
+            }
+            if new_value.start <= new_value.end {
+                match condition.field {
+                    'x' => queue.push((&condition.jump_to, new_value, m.clone(), a.clone(), s.clone())),
+                    'm' => queue.push((&condition.jump_to, x.clone(), new_value, a.clone(), s.clone())),
+                    'a' => queue.push((&condition.jump_to, x.clone(), m.clone(), new_value, s.clone())),
+                    's' => queue.push((&condition.jump_to, x.clone(), m.clone(), a.clone(), new_value)),
+                    _ => panic!("Invalid field {}", condition.field)
+                }
+            }
+        }
+        queue.push((&rule.default, x, m, a, s));
+    }
+
+    for accept in accepted {
+        ans += (accept.0.end - accept.0.start + 1) as i64 * (accept.1.end - accept.1.start + 1) as i64 * (accept.2.end - accept.2.start + 1) as i64 * (accept.3.end - accept.3.start + 1) as i64;
     }
 
     println!("Part 2: {}", ans);
