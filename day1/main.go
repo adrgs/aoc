db199d5ef4e64f91ae5035f6cc20f55d7f6a3a51
adrgs <albastroiudragos@gmail.com>
2023-12-26 23:11:14 +0100

    Add day1 part 1

---
+++ day1/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func part1(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	previous := -1
+	ans := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+
+		if previous != -1 {
+			if number > previous {
+				ans += 1
+			}
+		}
+
+		previous = number
+
+	}
+
+	fmt.Println("Part 1: ", ans)
+}
+
+func part2(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	ans := 0
+	fmt.Println("Part 1: ", ans)
+}
+
+func main() {
+	part1("./input.txt")
+	part2("./input.txt")
+}
