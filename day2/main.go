95d675958b34598a804ddb0cb6e571b8170b5b78
adrgs <albastroiudragos@gmail.com>
2023-12-27 00:15:05 +0100

    Add day2 part 1

---
+++ day2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func part1(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	ans := 0
+	horizontal := 0
+	depth := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), " ")
+		switch data[0] {
+		case "forward":
+			number, _ := strconv.Atoi(data[1])
+			horizontal += number
+		case "up":
+			number, _ := strconv.Atoi(data[1])
+			depth -= number
+		case "down":
+			number, _ := strconv.Atoi(data[1])
+			depth += number
+		}
+	}
+
+	ans = horizontal * depth
+
+	fmt.Println("Part 1: ", ans)
+}
+
+func part2(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	ans := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	}
+
+	fmt.Println("Part 1: ", ans)
+}
+
+func main() {
+	part1("./input.txt")
+	part2("./input.txt")
+}
