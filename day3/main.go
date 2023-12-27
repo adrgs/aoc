e8b1e2807a49518faf411ecfc41264d38a56d175
adrgs <albastroiudragos@gmail.com>
2023-12-27 01:01:31 +0100

    Add day3

--- day3/main.go
+++ day3/main.go
@@ -48,6 +48,40 @@ func part1(filename string) {
 	fmt.Println("Part 1: ", ans)
 }
 
+func filter(i int, numbers []string, least bool) string {
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+
+	counter := 0
+
+	for _, bits := range numbers {
+		if bits[i] == '1' {
+			counter += 1
+		}
+	}
+
+	newNumbers := []string{}
+
+	for _, bits := range numbers {
+		if 2*counter >= len(numbers) {
+			if bits[i] == '1' && !least {
+				newNumbers = append(newNumbers, bits)
+			} else if bits[i] == '0' && least {
+				newNumbers = append(newNumbers, bits)
+			}
+		} else {
+			if bits[i] == '0' && !least {
+				newNumbers = append(newNumbers, bits)
+			} else if bits[i] == '1' && least {
+				newNumbers = append(newNumbers, bits)
+			}
+		}
+	}
+
+	return filter(i+1, newNumbers, least)
+}
+
 func part2(filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -56,13 +90,37 @@ func part2(filename string) {
 	defer file.Close()
 
 	ans := 0
+	numbers := []string{}
+	maxLength := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		bits := scanner.Text()
+		numbers = append(numbers, bits)
 
+		maxLength = len(numbers[len(numbers)-1]) // assume all lines are the same length
 	}
 
-	fmt.Println("Part 1: ", ans)
+	o2 := filter(0, numbers, false)
+	co2 := filter(0, numbers, true)
+
+	numberO2 := 0
+	numberCO2 := 0
+
+	for i := 0; i < maxLength; i++ {
+		numberO2 *= 2
+		numberCO2 *= 2
+		if o2[i] == '1' {
+			numberO2 += 1
+		}
+		if co2[i] == '1' {
+			numberCO2 += 1
+		}
+	}
+
+	ans = numberO2 * numberCO2
+
+	fmt.Println("Part 2: ", ans)
 }
 
 func main() {
