02ed7fe69b58fc01e17951b13d80b1c5c54feac4
adrgs <albastroiudragos@gmail.com>
2024-01-16 00:02:43 +0100

    Add day7

--- day7/main.go
+++ day7/main.go
@@ -70,10 +70,33 @@ func part2(filename string) {
 	defer file.Close()
 
 	ans := 0
+	crabs := make([]int, 0)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		//fmt.Println(scanner.Text())
+		for _, c := range strings.Split(scanner.Text(), ",") {
+			number, err := strconv.Atoi(c)
+			if err != nil {
+				continue
+			}
+			crabs = append(crabs, number)
+		}
+	}
+
+	// sort the crabs
+	sort.Ints(crabs)
+
+	// given len(crabs) == 1000, O(N^2) is fine
+
+	ans = 1e9 + 7
+
+	for i := 0; i < len(crabs); i++ {
+		sum := 0
+		for j := 0; j < len(crabs); j++ {
+			n := abs(crabs[i] - crabs[j])
+			sum += (n * (n + 1)) / 2
+		}
+		ans = min(ans, sum)
 	}
 
 	fmt.Println("Part 2:", ans)
